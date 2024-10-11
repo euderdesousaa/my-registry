497fa32ae9ad99d3f0cf815388607a699c6c8a33
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-11 19:46:16 -0300

    a

--- jwt-utils/src/main/java/tech/engix/jwtutils/service/JwtUtils.java
+++ jwt-utils/src/main/java/tech/engix/jwtutils/service/JwtUtils.java
@@ -3,6 +3,7 @@ package tech.engix.jwtutils.service;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -10,9 +11,10 @@ import javax.crypto.SecretKey;
 @Service
 public class JwtUtils {
 
-
+    @Value("${jwtSecret}")
+    private String jwtSecret;
     private SecretKey key() {
-        String jwtSecret = "k8HOg7nyIr396UG8T3DPuSEHaX9sFg67rve3aOhB0+8=";
+
         byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
